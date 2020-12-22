04d1c467545196c36215cd01d097076de6fdee51
serhiistr <sergaudit2019@gmail.com>
2020-12-22 11:10:25 +0200

    22122020

---
+++ test42.py
@@ -0,0 +1,8 @@
+# Слово по буквам
+# Демонсрирует применение цикла for к строке
+word = input("Enter a word: ")
+print("\n This is all letters in order: ")
+for letter in word:
+    print(letter)
+
+input("\n\n Click Enter to leave.")

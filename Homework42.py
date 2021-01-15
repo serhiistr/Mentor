6cd59e7d83243c71e2d838f6b48b7cbbb1ebd56a
serhiistr <sergaudit2019@gmail.com>
2021-01-15 14:15:04 +0200

    15012021

--- Homework42.py
+++ Homework42.py
@@ -1,14 +1,20 @@
 # Напишите программу, которая принимала бы текст из пользовательского ввода
 # и печатала этот текст на экране наоборот
 
+import random
+
 word = input("Введите слово: ")
+
+word_1 = word
 
 new_word = " "
 
-while word:
+while word_1:
+    for i in range(-len(word), 0, 1):
+        new_word = new_word + word[i]
+    position = random.randrange(len(word_1))
+    word1 = word_1[:position] + word_1[(position + 1):]
 
-
-
-
+print(new_word)
 
 input("Click Enter to leave")

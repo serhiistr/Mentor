eceda90d9a6164da5100f2241a787183d4d6c9ee
serhiistr <sergaudit2019@gmail.com>
2021-09-21 15:38:36 +0300

    21092021

---
+++ test64.py
@@ -0,0 +1,34 @@
+# Доступ отовсюду
+# Демонстрирует работу с глобальными переменными
+
+def read_global():
+    print("В области видимости функции read_global() значение value равно", value)
+
+
+def shadow_global():
+    value = -10
+    print("В области видимости функции shadow_global() значение value равно", value)
+
+
+def change_global():
+    global value
+    value = -10
+    print("В области видимости функции change_global() значение value равно", value)
+
+# основная часть
+# value - глобальная переменная, потому что мы находимся в глобальной области видимости
+
+
+value = 10
+print("В глобальной области видимости значение переменной value сейчас стало равным", value, "\n")
+
+read_global()
+print("Вернемся в глобальную область видимости. Здесь value по-прежнему равно", value, "\n")
+
+shadow_global()
+print("Вернемся в глобальную область видимости. Здесь value по-прежнему равно", value, "\n")
+
+change_global()
+print("Вернемся в глобальную область видимости. Здесь value изменилось на", value, "\n")
+
+input("\n\nClick Enter to leave")

e6324d1d0046df065b5e776483d4982f4e5c42ce
serhiistr <sergaudit2019@gmail.com>
2021-09-29 09:46:12 +0300

    29092021

---
+++ constitutoin.py
@@ -0,0 +1,10 @@
+file = open("C:/Users/admin/PycharmProjects/Mentor/constitution.txt", "r")
+# print(file.read())
+
+# for line in file:
+#     print(line)
+
+file.close()
+
+file.read("C:/Users/admin/PycharmProjects/Mentor/constitution.txt", "r")
+print(file.read())
---
+++ practemp.py
@@ -0,0 +1,15 @@
+# Написать функцию is_year_leap, принимающую 1 аргумент — год, и
+# возвращающую True, если год високосный, и False иначе.
+
+a = int(input("Please enter the year for the test: "))
+b = a % 4
+
+
+def leap_year():
+    if b == 0:
+        print(b == 0)
+    if b != 0:
+        print(b == 0)
+
+
+leap_year()

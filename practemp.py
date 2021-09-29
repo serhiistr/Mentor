cbe20a18e846d214af5a22e88c20c057cb21d073
serhiistr <sergaudit2019@gmail.com>
2021-09-29 10:12:53 +0300

    29092021

--- practemp.py
+++ practemp.py
@@ -4,12 +4,15 @@
 a = int(input("Please enter the year for the test: "))
 b = a % 4
 
-
-def leap_year():
-    if b == 0:
-        print(b == 0)
-    if b != 0:
-        print(b == 0)
+while a != 0:
 
 
-leap_year()
+
+    def leap_year():
+        if b == 0:
+            print(b == 0)
+        if b != 0:
+            print(b == 0)
+
+
+    leap_year()

f318366f4cb1e37e42b8a1d326e41208aa007f9d
serhiistr <sergaudit2019@gmail.com>
2021-09-29 13:49:55 +0300

    29092021

--- temptemptemp.py
+++ temptemptemp.py
@@ -9,3 +9,20 @@ print(a.split())
 s = ['43', '54', '32', '65']
 print('+'.join(s))
 print(''.join([str(i) for i in s]))
+
+
+a = [-1, -2, -3, 4, 5]
+b = list(map(abs, a))
+c = [ abs(i) for i in a]
+print(b, c)
+
+
+def f(x):
+    return x**2
+b = list(map(f, a))
+print(b)
+
+a = ['hello', 'hi', 'good morning']
+b = list(map(str.upper, a))
+#map используют не только с фукциями (str и другие) но и с методами (upper и другие)
+print(b)

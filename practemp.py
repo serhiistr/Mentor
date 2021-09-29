e3bb645a8e21ca8dd8e021bb2b2813a0d9ffae46
serhiistr <sergaudit2019@gmail.com>
2021-09-29 11:24:35 +0300

    29092021

--- practemp.py
+++ practemp.py
@@ -1,18 +1,24 @@
 # Написать функцию is_year_leap, принимающую 1 аргумент — год, и
 # возвращающую True, если год високосный, и False иначе.
+print("Введите год для проверки, для выхода из программы нажмите '0'\n")
+
 
 a = int(input("Please enter the year for the test: "))
-b = a % 4
+# b = a % 4
+
+
+def leap_year():
+    if b == 0:
+        print(b == 0)
+    if b != 0:
+        print(b == 0)
+
+
+# leap_year()
 
 while a != 0:
-
-
-
-    def leap_year():
-        if b == 0:
-            print(b == 0)
-        if b != 0:
-            print(b == 0)
-
-
+    b = a % 4
     leap_year()
+    a = int(input("Please enter the year for the test: "))
+
+# input("\n\n Click Enter to leave")

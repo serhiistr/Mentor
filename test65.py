db5509fdc6c85b57eaa3a4540bc83108f1d7b808
serhiistr <sergaudit2019@gmail.com>
2021-10-08 15:59:24 +0300

    08102021

--- test65.py
+++ test65.py
@@ -65,3 +65,41 @@ def new_board():
     return board
 
 
+def display_board(board):
+    """Отображает игровую доску на экране"""
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\t", board[6], "|", board[7], "|", board[8], "\n")
+
+
+def legal_moves(board):
+    """Создает список доступных ходов"""
+    moves = []
+    for square in range(NUM_SQUARES):
+        if board[square] == EMPTY:
+            moves.append(square)
+    return moves
+
+
+def winner(board):
+    """Определяет победителя в игре"""
+    WAYS_TO_WIN = ((0, 1, 2),
+                   (3, 4, 5),
+                   (6, 7, 8),
+                   (0, 3, 6),
+                   (1, 4, 7),
+                   (2, 5, 8),
+                   (0, 4, 8),
+                   (2, 4, 6))
+    for row in WAYS_TO_WIN:
+        if board[row[0]] == board[row[1]] == board[row[2]] !=EMPTY:
+            winner = board[row[0]]
+            return winner
+        if EMPTY not in board:
+            return TIE
+    return None
+
+
+def

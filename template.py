f97b2acb6f47a4960d9b4590c6329f7bab884387
serhiistr <sergaudit2019@gmail.com>
2021-10-18 16:36:44 +0300

    18102021

--- template.py
+++ template.py
@@ -4,10 +4,39 @@
 #     print(word[i])
 
 
-def main():
-    print("Hello world")
+# def main():
+#     print("Hello world")
+#
+#
+# main()
+#
+# print("OK")
+
+EMPTY = " "
+NUM_SQUARES = 9
 
 
-main()
+def new_board():
+    """Создает новую игровую доску."""
+    board = []
+    for square in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
 
-print("OK")
+
+board = new_board()
+
+
+def display_board(board):
+    """Отображает игровую доску на экране"""
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\t", board[6], "|", board[7], "|", board[8], "\n")
+
+
+display_board(board)
+
+
+

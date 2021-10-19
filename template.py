ce292fffed2c98ce05f199c892cd2ff33eed19ce
serhiistr <sergaudit2019@gmail.com>
2021-10-19 13:52:56 +0300

    19102021

--- template.py
+++ template.py
@@ -16,27 +16,37 @@ EMPTY = " "
 NUM_SQUARES = 9
 
 
-def new_board():
-    """Создает новую игровую доску."""
-    board = []
-    for square in range(NUM_SQUARES):
-        board.append(EMPTY)
-    return board
+# def new_board():
+#     """Создает новую игровую доску."""
+#     board = []
+#     for square in range(NUM_SQUARES):
+#         board.append(EMPTY)
+#     return board
+#
+#
+# board = new_board()
+#
+#
+# def display_board(board):
+#     """Отображает игровую доску на экране"""
+#     print("\n\t", board[0], "|", board[1], "|", board[2])
+#     print("\t", "---------")
+#     print("\t", board[3], "|", board[4], "|", board[5])
+#     print("\t", "---------")
+#     print("\t", board[6], "|", board[7], "|", board[8], "\n")
+#
+#
+# display_board(board)
 
 
-board = new_board()
+if 1:
+    print("TRUE")
+
+if 0:
+    print("FALSE")
 
 
-def display_board(board):
-    """Отображает игровую доску на экране"""
-    print("\n\t", board[0], "|", board[1], "|", board[2])
-    print("\t", "---------")
-    print("\t", board[3], "|", board[4], "|", board[5])
-    print("\t", "---------")
-    print("\t", board[6], "|", board[7], "|", board[8], "\n")
-
-
-display_board(board)
-
+print("Result: ", 5, 7, ".", sep="", end="")
+print("Second \" line")
 
 

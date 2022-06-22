c3d28e0f77abc08ab443b1ded3be58d14a5cb1fc
serhiistr <sergaudit2019@gmail.com>
2022-06-22 18:05:41 +0300

    22062022

--- factorial.py
+++ factorial.py
@@ -1,9 +1,25 @@
-
-
 def fact(x):
-    if x == 1:
+    if x == 0:
+        return 1
+    elif x == 1:
         return 1
     return fact(x-1)*x
 
 
-print(fact(3))
+def factor(x):
+    if x == 0:
+        return 1
+    elif x == 1:
+        return 1
+    return factor(x-1)*x
+
+
+a = int(input("Enter a number: "))
+
+while a >= 0:
+    print(fact(a))
+    print(factor(a))
+    a = int(input("Enter a number: "))
+
+
+
